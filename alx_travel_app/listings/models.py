60f30cb71e6ecf8b1b927ebeb544e266cf801f9d
Azegba Kizito Amandianeze <kizitoamandi96@gmail.com>
2025-08-15 13:08:47 +0100

    Added a README.md file

---
+++ alx_travel_app/listings/models.py
@@ -0,0 +1,53 @@
+from django.db import models
+
+class Listing(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    price_per_night = models.DecimalField(max_digits=10, decimal_places=2)
+    location = models.CharField(max_length=255)
+    available_from = models.DateField()
+    available_to = models.DateField()
+
+    def __str__(self):
+        return self.title
+
+
+class Booking(models.Model):
+    listing = models.ForeignKey(Listing, related_name='bookings', on_delete=models.CASCADE)
+    guest_name = models.CharField(max_length=255)
+    guest_email = models.EmailField()
+    check_in = models.DateField()
+    check_out = models.DateField()
+
+    def __str__(self):
+        return f"{self.guest_name} - {self.listing.title}"
+
+
+class Review(models.Model):
+    listing = models.ForeignKey(Listing, related_name='reviews', on_delete=models.CASCADE)
+    reviewer_name = models.CharField(max_length=255)
+    rating = models.IntegerField()
+    comment = models.TextField()
+
+    def __str__(self):
+        return f"{self.reviewer_name} - {self.listing.title}"
+
+from django.db import models
+
+
+class Payment(models.Model):
+    STATUS_CHOICES = [
+        ('Pending', 'Pending'),
+        ('Completed', 'Completed'),
+        ('Failed', 'Failed'),
+    ]
+
+    booking_reference = models.CharField(max_length=100)
+    transaction_id = models.CharField(max_length=255, blank=True, null=True)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='Pending')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.booking_reference} - {self.status}"
+
